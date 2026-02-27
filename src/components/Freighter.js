a67a6505659270333e1c8b13dbe410e9cafecbc6
Rishav Roy <rishavroy2005@gmail.com>
2026-02-28 00:13:16 +0530

    Refactor Freighter.js for improved readability

--- src/components/Freighter.js
+++ src/components/Freighter.js
@@ -1,29 +1,37 @@
-import { signTransaction, setAllowed, getAddress }
-from '@stellar/freighter-api';
-import * as StellarSdk from '@stellar/stellar-sdk';
-const server=new StellarSdk.Horizon.Server('https://horizon-testnet.stellar.org');
+import {
+  signTransaction,
+  setAllowed,
+  getAddress,
+  disconnect,
+} from "@stellar/freighter-api";
+import * as StellarSdk from "@stellar/stellar-sdk";
+
+const server = new StellarSdk.Horizon.Server(
+  "https://horizon-testnet.stellar.org"
+);
+
 const checkConnection = async () => {
-    return await setAllowed();
-}
+  return await setAllowed();
+};
 
 const retrievePublicKey = async () => {
-    const { address } = await getAddress();
-    return address;
+  const { address } = await getAddress();
+  return address;
 };
 
 const getBalance = async () => {
-    await setAllowed();
-    const { address } = await getAddress();
-    const account = await server.loadAccount(address);
-    const xlm=account.balances.find((b)=>b.asset_type==="native");
-    return xlm? xlm.balance : "0";
+  await setAllowed();
+  const { address } = await getAddress();
+  const account = await server.loadAccount(address);
+  const xlm = account.balances.find((b) => b.asset_type === "native");
+  return xlm ? xlm.balance : "0";
 };
 
-const userSignTransaction=async(xdr,network,signWith)=>{
-    return await signTransaction(xdr,{
-        network,
-        accountToSign: signWith,
-    });
+const userSignTransaction = async (xdr, network, signWith) => {
+  return await signTransaction(xdr, {
+    network,
+    accountToSign: signWith,
+  });
 };
 
 const sendPayment = async (destination, amount) => {
@@ -65,10 +73,18 @@ const sendPayment = async (destination, amount) => {
   return res.hash;
 };
 
+const disconnectWalletApi = async () => {
+  try {
+    await disconnect();
+  } catch (_) {
+  }
+};
+
 export {
   checkConnection,
   retrievePublicKey,
   getBalance,
   userSignTransaction,
   sendPayment,
+  disconnectWalletApi,
 };
